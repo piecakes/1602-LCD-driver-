fbf2df83b2449a05c3d7bb8b25b3541c81a3eae4
Jenny Thorne <jennythorne3006@gmail.com>
2015-10-19 16:41:30 +0100

    fixing typos

--- main.py
+++ main.py
@@ -2,15 +2,14 @@ from LCD_1602 import lcddriver
 import time
 
 def main():
-    while True:
         lcd = lcddriver.lcd()
-        lcd.backlight(0)
         lcd.lcd_display_string("Hello world!", 1)
         lcd.lcd_display_string("My name is Pi", 2)
-        time.sleep(1)
-        lcd.lcd_clear()
-        time.sleep(1)
+        time.sleep(30)
 
 if __name__ == "__main__":
     main()
+
+
+
 
